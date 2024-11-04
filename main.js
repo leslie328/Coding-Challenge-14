a0604acefc383942a442f70038f9b0fb7fbd566d
Leslie Rodriguez <leslie286@usf.edu>
2024-11-03 23:00:01 -0500

    Dynamically display tickets on the page

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ async function fetchUnresolvedTickets() {
         if (tickets.length === 0) {
             throw new Error('No unresolved tickets found.');
         }}}
+
+        //Task 3: Display Tickets Dynamically on the Page
+        function displayTickets(tickets) {
+    const container = document.getElementById('ticket-container');
+  
+tickets.forEachticket => {
+        const ticketDiv = document.createElement('div');
+        ticketDiv.classList.add('ticket');
+        ticketDiv.innerHTML = `
+            <h2>Ticket ID: ${ticket.id}</h2>
+            <p><strong>Customer Name:</strong> User ${ticket.userId}</p>
+            <p><strong>Issue Description:</strong> ${ticket.title}</p>
+            <p><strong>Details:</strong> ${ticket.body}</p>
+        `;
+        
+        }};
